966849e4e9760f0d6e2633b4176c51c3533314df
bashir-ahmed <babashir8811@gmail.com>
2021-07-07 15:10:24 +0600

    bootcamp site

--- app.js
+++ app.js
@@ -4,9 +4,9 @@ const bars = document.querySelector(".fa-bars");
 bars.addEventListener("click", () => {
 	navbar.classList.toggle("show");
 });
-
+//git remote add origin https://github.com/bashir-CSE/FrontedBootcamp.github.io.git
 // mapbox
-mapboxgl.accessToken =
+https: mapboxgl.accessToken =
 	"pk.eyJ1IjoiYmFzaGlybWFwIiwiYSI6ImNrcTlkbW9uODAweHIydW11aTF6azExMzAifQ.SZY3jdO-lXjyadYr86pJog";
 var map = new mapboxgl.Map({
 	container: "map",
