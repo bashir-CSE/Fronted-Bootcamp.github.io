9ed1f2c6fafaa3a3ab94c5bfe80a947332536c3f
bashir-ahmed <babashir8811@gmail.com>
2021-07-07 15:14:36 +0600

    FrontedBootcamp site

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const bars = document.querySelector(".fa-bars");
 bars.addEventListener("click", () => {
 	navbar.classList.toggle("show");
 });
-//git remote add origin https://github.com/bashir-CSE/FrontedBootcamp.github.io.git
+//git remote add origin https://github.com/bashir-CSE/FrontedBootcamp.github.io.git FrontedBootcamp.github.io
 // mapbox
 https: mapboxgl.accessToken =
 	"pk.eyJ1IjoiYmFzaGlybWFwIiwiYSI6ImNrcTlkbW9uODAweHIydW11aTF6azExMzAifQ.SZY3jdO-lXjyadYr86pJog";
