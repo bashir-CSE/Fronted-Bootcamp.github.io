1b7d36991ffdc25519327f0d7337c145e5ae2711
bashir-ahmed <babashir8811@gmail.com>
2021-07-07 15:07:12 +0600

    bootcamp site

---
+++ app.js
@@ -0,0 +1,20 @@
+// custome responsive navbar
+const navbar = document.querySelector(".navbar-nav");
+const bars = document.querySelector(".fa-bars");
+bars.addEventListener("click", () => {
+	navbar.classList.toggle("show");
+});
+
+// mapbox
+mapboxgl.accessToken =
+	"pk.eyJ1IjoiYmFzaGlybWFwIiwiYSI6ImNrcTlkbW9uODAweHIydW11aTF6azExMzAifQ.SZY3jdO-lXjyadYr86pJog";
+var map = new mapboxgl.Map({
+	container: "map",
+	style: "mapbox://styles/mapbox/streets-v11",
+	center: [-104.9, 39.7],
+	zoom: 9,
+});
+// AOS
+AOS.init({
+	duration: 1000,
+});
